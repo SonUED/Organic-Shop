a73f1f634ddef5a7d1019c0a67ba2cbfe2727024
SonUED <45086369+SonUED@users.noreply.github.com>
2019-12-27 00:37:51 +0700

    Organic Shop
    Organic Shop

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: true,
+  firebase : {
+    apiKey: "AIzaSyCHpeRlwHPgY_bcV2Kw9uDCcNp0DZm9AQg",
+    authDomain: "webbanhang-c850b.firebaseapp.com",
+    databaseURL: "https://webbanhang-c850b.firebaseio.com",
+    projectId: "webbanhang-c850b",
+    storageBucket: "webbanhang-c850b.appspot.com",
+    messagingSenderId: "764214330368",
+    appId: "1:764214330368:web:38d2522746ab69970984e5",
+    measurementId: "G-M1KDLX7QGC"
+  }
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,17 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDGO_pejyD0A8kTjVO8K2Qp3vMKdCQS78M",
+    authDomain: "sonbk-project.firebaseapp.com",
+    databaseURL: "https://sonbk-project.firebaseio.com",
+    projectId: "sonbk-project",
+    storageBucket: "sonbk-project.appspot.com",
+    messagingSenderId: "13582304650",
+    appId: "1:13582304650:web:462ec249c6ec33ad317473",
+    measurementId: "G-3ZD2SPRBM4"
+  }
+};
